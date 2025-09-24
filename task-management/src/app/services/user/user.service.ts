00d9e43b4f4c4ad73d6b12505a3403f0807b77f3
Hope M Kugotsi <hopekugotsi@gmail.com>
2025-09-24 15:44:50 +0200

    ready for qat

--- task-management/src/app/services/user/user.service.ts
+++ task-management/src/app/services/user/user.service.ts
@@ -3,17 +3,19 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { RegisterRequest, RegisterSuccessResponse } from '../../models/user';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private readonly baseUrl = 'http://localhost:9003';
+  private  baseUrl = environment.baseUrl;
+  private userFullUrl = this.baseUrl + '/auth/register';
 
   constructor(private http: HttpClient) { }
 
   register(payload: RegisterRequest): Observable<RegisterSuccessResponse> {
-    const url = `${this.baseUrl}/auth/register`;
+    const url = `${this.userFullUrl}`;
     return this.http.post<RegisterSuccessResponse>(url, payload).pipe(
       catchError((error: HttpErrorResponse) => throwError(() => error))
     );
